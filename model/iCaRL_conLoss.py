acedb3406c572ad168ba6c36a16383efea197adb
Andres Villa <ojedaf@uninorte.edu.co>
2022-07-04 11:06:29 -0400

    Update iCaRL_conLoss.py

--- model/iCaRL_conLoss.py
+++ model/iCaRL_conLoss.py
@@ -17,8 +17,10 @@ from torch.cuda.amp import autocast
 class iCaRL(nn.Module):
     def __init__(self, conf_model, num_class, conf_checkpoint):
         super(iCaRL, self).__init__()
-		
+
+        # Model Checkpoint
         self.conf_checkpoint = conf_checkpoint
+        # Model Config
         self.conf_model = conf_model
         num_segments = conf_model['num_segments']
         modality = conf_model['modality']
@@ -34,6 +36,7 @@ class iCaRL(nn.Module):
         self.fc_lr5 = conf_model['fc_lr5']
         temporal_pool = conf_model['temporal_pool']
         non_local = conf_model['non_local']
+        # Model backbone (TSN)
         self.feature_encoder = TSN(num_class, num_segments, modality, 
                                     base_model=arch,
                                     consensus_type=consensus_type,
@@ -45,10 +48,7 @@ class iCaRL(nn.Module):
                                     fc_lr5=self.fc_lr5,
                                     temporal_pool=temporal_pool,
                                     non_local=non_local)
-								   
-		# feature_dim = self.feature_encoder.new_fc.in_features
-	    # self.new_fc = nn.Linear(feature_dim, num_class)
-		
+
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.crop_size = self.feature_encoder.crop_size
         self.scale_size = self.feature_encoder.scale_size
@@ -70,8 +70,12 @@ class iCaRL(nn.Module):
         self.memory = {}
         self.list_val_acc_ii = []
         self.adv_lambda = conf_model['adv_lambda']
-        print('adv_lambda: ',self.adv_lambda)	
+        print('adv_lambda: ',self.adv_lambda)
+        self.type_sampling = conf_model['type_sampling']
+        print('model sampling strategy:', self.type_sampling)
 
+    # To prepare the model for a novel task, it is essential to augment the classifier according to the novel classes. 
+    # Thus, it is crucial to update the optimization policies.
     def get_optim_policies(self):
         if torch.cuda.device_count() > 1:
             return self.feature_encoder.module.get_optim_policies()
@@ -83,7 +87,10 @@ class iCaRL(nn.Module):
             return self.feature_encoder.module.get_augmentation(flip)
         else:
             return self.feature_encoder.get_augmentation(flip)
-
+    
+    # Function to augment the classification layer
+    # num_new_classes - Num of new classes.
+    # device - Pytorch device (GPU).
     def augment_classification(self, num_new_classes, device):
         if torch.cuda.device_count() > 1:
             return self.feature_encoder.module.augment_classification(num_new_classes, device)
@@ -91,37 +98,25 @@ class iCaRL(nn.Module):
             return self.feature_encoder.augment_classification(num_new_classes, device)
         
         self.n_classes += num_new_classes
-        
+    
+    # Set the losses
+    # cls_loss - Classification Loss
+    # dist_loss - Distillation Loss
     def set_losses(self, cls_loss, dist_loss):
         self.cls_loss = cls_loss
         self.dist_loss = dist_loss
     
-	# def augment_classification(self, num_new_classes):
-        # # add in the policies
-        # feature_dim = self.new_fc.in_features
-        # out_class = self.new_fc.out_features
-        # out_dim = out_class + num_new_classes
-        # weight = self.new_fc.weight.data
-        # bias = self.new_fc.bias.data
-        # new_fc = nn.Linear(feature_dim, out_dim).to(self.device)
-        # std = 0.001
-        # if hasattr(new_fc, 'weight'):
-            # normal_(new_fc.weight, 0, std)
-            # constant_(new_fc.bias, 0)
-        
-        # new_fc.weight.data[:out_class] = weight[:out_class]
-        # new_fc.bias.data[:out_class] = bias[:out_class]
-        
-        # self.new_fc = new_fc
-    
+    # Function to do a forward pass through the model, including the linear layer.
     def forward(self, x):
         x = self.feature_encoder(x, get_emb = False)
-        # x = self.new_fc(x)
         return x
-        
-    def add_samples_to_mem(self, cilsettask, data, m, type_sampling = 'icarl'): 
-        # verificar que los elementos de videos vengan en el mismo orden de class_loader
-        if type_sampling == 'icarl':
+    
+    # Function to manage the memory.
+    # Data - It is candidate data to save into memory.
+    # m - The number of elements per class in the memory.
+    def add_samples_to_mem(self, cilsettask, data, m): 
+        # Memory sampling strategy of iCaRL.
+        if self.type_sampling == 'icarl':
             for class_id, videos in data.items():
                 data_class = {class_id:videos}
                 class_loader = cilsettask.get_dataloader(data_class, sample_frame = True)
@@ -183,15 +178,21 @@ class iCaRL(nn.Module):
             
             self.memory = {class_id: videos[:m] for class_id, videos in self.memory.items()}
         else:
+            # Random Memory Sampling
             self.memory = {**self.memory, **data}
             for class_id, videos in self.memory.items():
                 random.shuffle(videos)
-                self.memory[class_id] = videos[:m]
-        
+                if m != 'ALL':
+                    self.memory[class_id] = videos[:m]
+                else:
+                    self.memory[class_id] = videos
+                    
         for class_id, videos in self.memory.items():
             print('Memory... Class: {}, num videos: {}'.format(class_id, len(videos)))
                 
-        
+    # This function is to classify the instances following the iCaRL pipeline.
+    # x - Batch to classify
+    # cilsettask - the class that handles the validation data loaders.
     def classify(self, x, cilsettask):
        
         batch_size = x.size(0)
@@ -222,8 +223,8 @@ class iCaRL(nn.Module):
         means = means.transpose(1, 2) # (batch_size, feature_size, n_classes)
 
         feature = self.feature_encoder(x, get_emb = True).cpu() # (batch_size, feature_size)
-        for i in range(feature.size(0)): # Normalize
-            feature.data[i] = feature.data[i] / feature.data[i].norm()
+        for i in range(feature.size(0)): 
+            feature.data[i] = feature.data[i] / feature.data[i].norm()    # Normalize
         feature = feature.unsqueeze(2) # (batch_size, feature_size, 1)
         feature = feature.expand_as(means) # (batch_size, feature_size, n_classes)
 
@@ -234,6 +235,7 @@ class iCaRL(nn.Module):
 
         return preds
     
+    # Function to load the best checkpoint of the model.
     def load_best_checkpoint(self, path_model, current_task):
         path_best_model = path_model.format('Best_Model')
         if os.path.exists(path_best_model):
@@ -242,6 +244,10 @@ class iCaRL(nn.Module):
             if task_to_load == current_task:
                 self.feature_encoder.load_state_dict(checkpoint_dict['state_dict'])
     
+    # Function to save the checkpoint of the model.
+    # dict_to_save - A dictionary with essential model configuration for restoring the model.
+    # path_model - The memory path to save the checkpoint.
+    # is_best - True if with this configuration, the model achieves the best accuracy until the moment.
     def save_checkpoint(self, dict_to_save, path_model, is_best):
         if is_best:
             print('Saving ... ')  
@@ -249,7 +255,8 @@ class iCaRL(nn.Module):
             torch.save(dict_to_save, best_model)
             print("Save Best Networks for task: {}, epoch: {}".format(dict_to_save['current_task'] + 1, 
                                                                  dict_to_save['current_epoch'] + 1), flush=True)
-                                                                 
+    
+    # Function to validate the model while it is trained.
     def validate(self, val_cilDatasetList, current_task_id):
         top1 = AverageMeter()
         total_acc = AverageMeter()
@@ -279,6 +286,7 @@ class iCaRL(nn.Module):
         print(output)
         return total_acc.avg
     
+    # Function to analyse the output of the model.
     def final_validation_analysis(self, val_cilDatasetList, current_task_id):
         top1 = AverageMeter()
         total_acc = AverageMeter()
@@ -293,17 +301,18 @@ class iCaRL(nn.Module):
             for n_task, (val_loader, num_classes) in enumerate(val_loaders_list):
                 list_pred_elems = []
                 for _, _, videos, _, target in val_loader:
-                    # target = target.to(self.device)
                     videos = videos.to(self.device)
                     # compute output
-                    preds = self.classify(videos, val_cilDatasetList)
-
-                    # check the accuracy function
-                    correct = (preds.data.cpu() == target).sum()
-                    acc_val = (100 * correct / target.size(0))
-                    # acc_val = accuracy(preds.data, target, topk=(1,))[0]
-
-                    # top1.update(acc_val.item(), videos.size(0))
+                    if self.type_sampling == 'icarl':
+                        preds = self.classify(videos, val_cilDatasetList)
+                        correct = (preds.data.cpu() == target).sum()
+                        acc_val = (100 * correct / target.size(0))
+                    else:
+                        target = target.to(self.device)
+                        preds = self.forward(videos)
+                        acc_val = accuracy(preds.data, target, topk=(1,))[0]
+                        acc_val = acc_val.item()
+                        
                     top1.update(acc_val, videos.size(0))
                     list_pred_elems.append({'targets': target,'preds':preds})
 
@@ -316,7 +325,10 @@ class iCaRL(nn.Module):
             print(output)
         return list_pred_elems_tasks
     
-        
+    # Function to evaluate the model at the end of the training process of each task using the metrics (Acc and BWF).
+    # val_cilDatasetList - Class that handles the validation data loaders.
+    # current_task_id - Id of the last learned task.
+    # experiment - commet object.
     def final_validate(self, val_cilDatasetList, current_task_id, experiment):
         top1 = AverageMeter()
         total_acc = AverageMeter()
@@ -329,17 +341,17 @@ class iCaRL(nn.Module):
         with torch.no_grad():
             for n_task, (val_loader, num_classes) in enumerate(val_loaders_list):
                 for _, _, videos, _, target in val_loader:
-                    # target = target.to(self.device)
                     videos = videos.to(self.device)
-                    # compute output
-                    preds = self.classify(videos, val_cilDatasetList)
+                    if self.type_sampling == 'icarl':
+                        preds = self.classify(videos, val_cilDatasetList)
+                        correct = (preds.data.cpu() == target).sum()
+                        acc_val = (100 * correct / target.size(0))
+                    else:
+                        target = target.to(self.device)
+                        preds = self.forward(videos)
+                        acc_val = accuracy(preds.data, target, topk=(1,))[0]
+                        acc_val = acc_val.item()
 
-                    # check the accuracy function
-                    correct = (preds.data.cpu() == target).sum()
-                    acc_val = (100 * correct / target.size(0))
-                    # acc_val = accuracy(preds.data, target, topk=(1,))[0]
-
-                    # top1.update(acc_val.item(), videos.size(0))
                     top1.update(acc_val, videos.size(0))
 
                 experiment.log_metric("Acc_task_{}".format(n_task+1), top1.avg, step=current_task_id+1)
@@ -373,6 +385,14 @@ class iCaRL(nn.Module):
                 self.feature_encoder.partialBN(True)
 
     
+    # Training function
+    # dataloader_cil - data loader of a task
+    # len_data - Number of training instances
+    # optimizer
+    # num_epochs - Number of epochs
+    # experiment - It is a Commet object essential to save relevant metrics. 
+    # task_id - task id
+    # val_cilDatasetList - List of validation data loaders.
     def train(self, dataloader_cil, len_data, optimizer, num_epochs, experiment, task_id, val_cilDatasetList):
 
         self.compute_means = True
@@ -384,19 +404,22 @@ class iCaRL(nn.Module):
 
         with experiment.train():
 
-            # Store network outputs with pre-update para 
+            # Store network outputs before training for previous knowledge distillation.
             if torch.cuda.device_count() > 1:
                 n_classes = self.feature_encoder.module.new_fc.out_features
             else:
                 n_classes = self.feature_encoder.new_fc.out_features
-            q = torch.zeros(len_data, n_classes).to(self.device)
-            for indices, _, videos, _, labels in dataloader_cil:
-                videos = videos.to(self.device)
-                indices = indices.to(self.device)
-                g = F.sigmoid(self.forward(videos))
-                q[indices] = g.data
-            q = Variable(q).to(self.device)
-
+                
+            if self.type_sampling == 'icarl':
+                q = torch.zeros(len_data, n_classes).to(self.device)
+                for indices, video_names, videos, _, labels in dataloader_cil:
+                    videos = videos.to(self.device)
+                    indices = indices.to(self.device)
+                    g = F.sigmoid(self.forward(videos))
+                    q[indices] = g.data
+                q = Variable(q).to(self.device)
+            
+            # Start training loop
             for epoch in range(num_epochs):
                 # switch to train mode
                 start = time.time()
@@ -410,22 +433,22 @@ class iCaRL(nn.Module):
                     labels = labels.to(self.device)
                     indices = indices.to(self.device)
                     videos_aug = videos_aug.to(self.device)
-
                     optimizer.zero_grad()
                     with autocast():
+                        # forward pass with the original video clips
                         g = self.forward(videos)
+                        # forward pass with its temporally downsampled versions
                         g_aug = self.forward(videos_aug)
                         
+                        # Temporal Consistency Loss
                         loss = get_robust_loss(self.cls_loss, g, g_aug, labels, adv_lambda=self.adv_lambda, 
                                                cr_lambda=0, l_outputs=None, l_outputs_aug=None)
 
-		    	# Classification loss for new classes
-#                         loss = self.cls_loss(g, labels)
+                        # Classification Accuracy
                         acc_train = accuracy(g.data, labels, topk=(1,))[0]
 
-		    	# Distilation loss for old classes
-                        if self.n_known > 0:
-                            # g = F.sigmoid(g)
+                        # Distilation loss for old classes
+                        if self.n_known > 0 and self.type_sampling == 'icarl':
                             q_i = q[indices]
                             dist_loss = sum(self.dist_loss(g[:,y], q_i[:,y])\
                                     for y in range(self.n_known))
@@ -448,11 +471,13 @@ class iCaRL(nn.Module):
                         print('Epoch [%d/%d], Loss: %.4f' 
                                %(epoch+1, num_epochs, loss.item()))
                 
+                # Register on Commet the final metrics for the epoch
                 experiment.log_metric("Epoch_Acc_task_{}".format(task_id+1), acc_Avg.avg)
                 experiment.log_metric("Epoch_Loss_task_{}".format(task_id+1), loss_Avg.avg)
                 end = time.time()
                 print('elapsed time: ',end-start)
 
+                # Evaluation loop
                 if (epoch + 1) % eval_freq == 0 or epoch == num_epochs - 1:
                     acc_val = self.validate(val_cilDatasetList, task_id)
                     is_best = acc_val >= best_acc_val
